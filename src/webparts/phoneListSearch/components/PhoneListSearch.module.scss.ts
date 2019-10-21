fcbcb95191f4476a931fdca97162687bc6e82072
Ben Brooks <ben@nutandem.com>
2019-10-21 07:48:39 -0700

    misc lingerers

---
+++ src/webparts/phoneListSearch/components/PhoneListSearch.module.scss.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+require("./PhoneListSearch.module.css");
+const styles = {
+  phoneListSearch: 'phoneListSearch_959d171a',
+  container: 'container_959d171a',
+  row: 'row_959d171a',
+  column: 'column_959d171a',
+  'ms-Grid': 'ms-Grid_959d171a',
+  title: 'title_959d171a',
+  subTitle: 'subTitle_959d171a',
+  description: 'description_959d171a',
+  button: 'button_959d171a',
+  label: 'label_959d171a',
+  contactItem: 'contactItem_959d171a',
+  contactItemImg: 'contactItemImg_959d171a',
+  contactItemDetails: 'contactItemDetails_959d171a',
+  contactItemFieldLabel: 'contactItemFieldLabel_959d171a',
+  contactItemFieldBody: 'contactItemFieldBody_959d171a',
+  contactItemFullName: 'contactItemFullName_959d171a',
+  padTop: 'padTop_959d171a',
+  padBottom: 'padBottom_959d171a',
+  small: 'small_959d171a',
+};
+
+export default styles;
+/* tslint:enable */
