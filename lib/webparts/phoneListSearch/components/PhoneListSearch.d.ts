ba0a06c5aaad2f31881531528aef541eabcc572f
Ben Brooks <ben@nutandem.com>
2019-11-15 04:36:47 -0800

    Ran gulp clean

--- lib/webparts/phoneListSearch/components/PhoneListSearch.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { IPhoneListSearchProps } from './IPhoneListSearchProps';
-export default class PhoneListSearch extends React.Component<IPhoneListSearchProps, {}> {
-    render(): React.ReactElement<IPhoneListSearchProps>;
-}
-//# sourceMappingURL=PhoneListSearch.d.ts.map
